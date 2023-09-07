7c46e5644791d86b15c09784b9e7727118e8839e
FloralPashtetKosuterro <hahatun2121@gmail.com>
2023-09-07 19:15:21 +0300

    Сделал 2 роута, один вычисляет сумму на джаваскрипт-моменте, а другой просто говорит, что мы попали не на ту страницу

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    if item_id == "2":
+        return{"Вы перешли куда-то не туда"}
+    else:
+        result = int(item_id + item_id)
+    return {"Результат сложения айди по джаваскрипту :^)": result}

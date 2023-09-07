7b185693d55e5cbb0085ccebbb4732761227c7ad
FloralPashtetKosuterro <hahatun2121@gmail.com>
2023-09-07 19:30:20 +0300

    Сдал изобретённый велосипед с типизацией не по документации FastAPI и добавил требования к библиотекам

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
-    item_id=int(item_id)
+async def read_item(item_id:int):
     if item_id > 1:
         return{f"Вы перешли куда-то не туда по id: {item_id}, попробуйте использовать id 1"}
     else:
